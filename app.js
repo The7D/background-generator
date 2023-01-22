9799f1c2e64fcf4ada4eb674e2f1b765c9df38a9
The7D <rajesh369d@gmail.com>
2023-01-22 23:46:36 +0530

    changes in html,css,js made

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var add = document.querySelector("#add");
 var taskInput = document.querySelector("#input");
-var ul = document.querySelector("ul");
+var temp;
+var ulTaskComplete = document.querySelector("#complete")
+var ul_incomplete = document.querySelector("#incomplete");
+
 var msg = document.querySelector(".alert");
 
 var gradient = document.querySelector("#gradient");
@@ -8,31 +11,34 @@ var color1 = document.querySelector(".colorOne");
 var color2 = document.querySelector(".colorTwo");
 var forH1bg = document.querySelector("h1");
 
-function isLengthOK() {
-    if (taskInput.value.length > 0) {
+const isLengthOK = () => {
+    if (taskInput.value.trim().length > 0) {
         createListItem();
     } else {
         alert("Please insert some task, then click on 'Add' Button");
-
+        taskInput.value = "";
     }
 }
 
 
-function chkForKeypress(event){
-    if (taskInput.value.length > 0 && event.keyCode === 13) {
+const chkForKeypress = event => {
+    if (taskInput.value.trim().length > 0 && event.keyCode === 13) {
         createListItem();
-    } else if (taskInput.value.length == 0 && event.keyCode === 13) {
+    } else if (taskInput.value.trim().length == 0 && event.keyCode === 13) {
         alert("Please insert some task, then click on 'Add' Button");
+        taskInput.value = "";
     }
 
 }
 
-function createListItem() {
+const createListItem = () => {
 
-    var li = document.createElement("li");
+    let li = document.createElement("li");
     li.classList.add("li-style");
     li.appendChild(document.createTextNode(taskInput.value.trim()));
-    ul.appendChild(li);
+    ul_incomplete.appendChild(li);
+    temp = taskInput.value;
+
     taskInput.value = "";
 
     li.addEventListener("click",function(){
@@ -42,22 +48,29 @@ function createListItem() {
 
     li.addEventListener("dblclick",function(){
         console.log("dbl working");
-        ul.removeChild(li);
+        // taskInput.value = ulTaskComplete.value;
+        console.log(temp, "hello");
+        
+        let liComplete = document.createElement("li");
+        liComplete.appendChild(document.createTextNode(temp));
+        ulTaskComplete.appendChild(liComplete);
+        
+        ul_incomplete.removeChild(li);
     })
 
 }
 
 
-function itemOnClick() {
+const itemOnClick = () => {
 
     isLengthOK();
 }
 
-function itemOnKeypress(event) {
+itemOnKeypress = event => {
     chkForKeypress(event);
 }
 
-function bgColor(){
+const bgColor = () => {
     gradient.style.background = 
     "linear-gradient( to right, "+ color1.value +","+ color2.value +")";
 
